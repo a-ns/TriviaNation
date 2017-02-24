5b849140e5c5fa31f5f3b78c23f2fc1dacbc13ee
AleksanderS <exmann94@gmail.com>
2017-02-23 22:40:47 -0600

    Added better typed example

--- socketIOCSharpClient/socketIOCSharpClient/Program.cs
+++ socketIOCSharpClient/socketIOCSharpClient/Program.cs
@@ -8,16 +8,20 @@ using Newtonsoft.Json;
 
 namespace socketIOCSharpClient
 {
+    public class Message
+    {
+        public string message { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            var socket = IO.Socket("http://localhost:3000");
+            Socket socket = IO.Socket("http://localhost:3000");
             
             socket.On("message", (data) => 
             {
-                dynamic theMessage = JsonConvert.DeserializeObject(data.ToString());
-                Console.WriteLine(theMessage.message);
+                Message response = JsonConvert.DeserializeObject<Message>(data.ToString());
+                Console.WriteLine(response.message);
             });
             Console.ReadKey();
         }
