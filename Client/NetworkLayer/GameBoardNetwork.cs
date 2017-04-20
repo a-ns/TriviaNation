0cede26736ec06490e698e6650bb6283f23a1081
Don Kerrigan <dak23@students.uwf.edu>
2017-04-20 14:22:09 -0500

    Added gameboard network interface and implementation

---
+++ Client/NetworkLayer/GameBoardNetwork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetworkLayerInterfaces;
+
+namespace NetworkLayer
+{
+    public class GameBoardNetwork : IGameBoardNetwork
+    {
+        private INetwork network;
+        public GameBoardNetwork(Network endpoint)
+        {
+            this.endpoint = endpoint;
+        }
+    }
+}
